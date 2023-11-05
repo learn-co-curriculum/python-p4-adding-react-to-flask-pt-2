4d3d82873986b1a2108926c897a72e6ccc7afa94
Linda Seiter <linseiter@gmail.com>
2023-11-05 09:10:00 -0500

    add client and server solution code, update Pipfile, readme instructions

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Check the console for a list of movies!</h1>;
+}
+
+export default App;
